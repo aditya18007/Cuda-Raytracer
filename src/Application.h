47c20f20648834bf1db1350ab7455579fe26e2de
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2022-04-07 23:54:41 +0530

    Code cleanup

--- src/Application.h
+++ src/Application.h
@@ -7,12 +7,14 @@
 
 #include <string>
 #include <GLFW/glfw3.h>
+#include "imgui.h"
 
 class Application {
 	const int m_width;
 	const int m_height;
 	static constexpr char const * m_title = "Helios";
 	static constexpr char const * m_glsl_version = "#version 330";
+	const ImVec4 m_clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	GLFWwindow* m_window;
 	
 private:
