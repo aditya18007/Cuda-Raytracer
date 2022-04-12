d37cca263e77ca32234711b92a6a11b2c08ed1d2
Aditya Singh Rathore <aditya18007@iiitd.ac.in>
2022-04-12 18:41:12 +0530

    Added thrust for easier cuda

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include "Dimensions.h"
 #include "Object_Loader.h"
 #include <iostream>
+#include "thrust/device_vector.h"
+#include "thrust/host_vector.h"
+
 int main(int, char**)
 {
-	
 	Object_Loader loader("backpack.obj");
 	Application a(WIDTH,HEIGHT );
 	a.run(loader);
+	
+	
 	return 0;
 }
