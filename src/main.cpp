efe278d110c1e830b3c88c526b9031934fbe2abc
Aditya Rathore <aditya18007@iiitd.ac.in>
2022-05-17 14:12:47 +0530

    Added model name reader

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "Application.h"
 #include "Dimensions.h"
 #include "Object_Loader.h"
+#include <iostream>
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
-	Object_Loader loader("cube.model");
+	if (argc < 2){
+		std::cout << "enter model name\n";
+		exit(-1);
+	}
+	std::string name(argv[1]);
+	Object_Loader loader(name);
 	Application a(WIDTH,HEIGHT );
 	a.load_model(loader);
 	a.run();
